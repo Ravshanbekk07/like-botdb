6d54a27c9f281467eec3b8a7fe25432f85017b87
djumanov <diyorbekjumanov1120@gmail.com>
2023-09-09 18:30:24 +0500

    inline like and dislike;

--- likebot/bot.py
+++ likebot/bot.py
@@ -1,7 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 import os
 from dotenv import load_dotenv
-from .handlers import start, help, like, dislike
+from .handlers import start, help, like, dislike, inline, like_query, dislike_query
 
 
 load_dotenv()
@@ -14,8 +14,11 @@ def main():
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("inline", inline))
     dispatcher.add_handler(MessageHandler(Filters.text("👍"), like))
     dispatcher.add_handler(MessageHandler(Filters.text("👎"), dislike))
+    dispatcher.add_handler(CallbackQueryHandler(pattern="dislike", callback=dislike_query))
+    dispatcher.add_handler(CallbackQueryHandler(pattern="like", callback=like_query))
 
     updater.start_polling()
     updater.idle()
--- likebot/handlers.py
+++ likebot/handlers.py
@@ -1,4 +1,4 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext
 from .db import DB
 
@@ -39,3 +39,47 @@ def dislike(update: Update, context: CallbackContext) -> None:
         text=f"you have {db.data[str(user.id)]['likes']} likes and {db.data[str(user.id)]['dislikes']} dislikes."
     )
 
+
+def inline(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_text(
+        text=f"you can press one of the buttons below",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("👍:0", callback_data="like:0"), InlineKeyboardButton("👎:0", callback_data="dislike:0")]],
+        )
+    )
+
+
+def like_query(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer("you pressed like")
+
+    likes = int(query.data.split(":")[1])
+    likes += 1
+
+    markup = update.callback_query.message
+
+    markup.reply_markup.inline_keyboard
+
+    query.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(f"👍:{likes}", callback_data=f"like:{likes}"), markup.reply_markup.inline_keyboard[0][1]]],
+        )
+    )
+
+def dislike_query(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer("you pressed dislike")
+
+    dislikes = int(query.data.split(":")[1])
+    dislikes += 1
+
+    markup = update.callback_query.message
+
+    markup.reply_markup.inline_keyboard
+
+    query.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(
+            [[markup.reply_markup.inline_keyboard[0][0], InlineKeyboardButton(f"👎:{dislikes}", callback_data=f"dislike:{dislikes}")]],
+        )
+    )
